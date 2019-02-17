c1f08c6ec6caff43469384179c5f9d3a5b3274dd
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-02-17 13:12:39 -0600

    code written no code for answer to the calculation in file

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,13 @@ namespace Calculator
         public static void Main()
         {
             int result = 0;
+            Console.Write("Please enter a number that you would like to use in our calculator problem: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a second number that you would like to use in our calculator problem: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Would you like to add, subtract, multiply or divide?");
+            Console.Write("Please enter which operation you would like to perform, in word form here: ")
 
             // Create a simple calculator that asks the user to input two different numbers 
             // and whether they want to add, subtract, multiply or divide them. 
