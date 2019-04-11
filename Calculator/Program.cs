b3186ead9cfef2dceded5b5d8706d0596487c8c0
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:19:21 -0500

    finish and pass tests

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,9 +12,32 @@ namespace Calculator
 
             Console.Write("Please enter a second number that you would like to use in our calculator problem: ");
             int secondNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Would you like to add, subtract, multiply or divide?");
-            Console.Write("Please enter which operation you would like to perform, in word form here: ")
 
+            Console.WriteLine("Would you like to add, subtract, multiply or divide?");
+            Console.Write("Please enter which operation you would like to perform, in word form here: ");
+            string operation = Console.ReadLine();
+
+            if (operation == "add")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if (operation == "subtract")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operation == "multiply")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operation == "divide")
+            {
+                result = firstNumber / secondNumber;
+            }
+            /*else
+            {
+                Console.WriteLine("Something was entered incorrectly.  Please press ENTER and try again.");
+            }
+            */
             // Create a simple calculator that asks the user to input two different numbers 
             // and whether they want to add, subtract, multiply or divide them. 
 
